c2646ab0a333f5bac6217f7cc7d70afef5f9071b
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2023-09-19 15:30:17 -0300

    Update server.R

--- server.R
+++ server.R
@@ -1,1 +1,31 @@
-# add server
+library(dplyr)
+
+server <- function(input, output){
+  
+  select_column_comp <- eventReactive(input$finish_c, {
+    df_column <- dataset 
+    return(df_column)
+  })
+  
+  output$timedate_c <- renderUI({
+    
+    column_name <- input$column_c
+    
+    df <- dataset
+    
+    dates <- df$date 
+ 
+    min_time <- (min(dates)) 
+    max_time <- (max(dates))
+    
+    dateRangeInput("true_date_comp", "Período de análise",
+                   end = max_time,
+                   start = min_time,
+                   min    = min_time,
+                   max    = max_time,
+                   format = "dd/mm/yy",
+                   separator = " - ",
+                   language='pt-BR')
+  })
+
+}
