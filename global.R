7578e4118f1f86f208e5cae486d24cd83d0760fa
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2023-09-20 13:36:52 -0300

    Update global.R

--- global.R
+++ global.R
@@ -1,3 +1,7 @@
+library(lubridate)
+library(ggplot2)
+library(purrr)
+library(hrbrthemes)
 
 dataset <- read.csv("south-korean-pollution-data.csv")
 columns <- c("pm25", "pm10", "o3", "no2", "so2", "co", "Lat", "Long", "City", "District")
