3ccf265754d4bf3285c58253d02270df234c2a2c
Lucas Vinícius <83307085+VS-Lucas@users.noreply.github.com>
2023-09-20 15:18:05 -0300

    Update global.R

--- global.R
+++ global.R
@@ -1,7 +1,20 @@
 library(lubridate)
 library(ggplot2)
 library(purrr)
+library(dplyr)
 library(hrbrthemes)
+library(RColorBrewer)
 
 dataset <- read.csv("south-korean-pollution-data.csv")
-columns <- c("pm25", "pm10", "o3", "no2", "so2", "co", "Lat", "Long", "City", "District")
+columns <- c("pm25", "pm10", "o3", "no2", "so2", "co", "Lat", "Long", "City", "District", "Country")
+cities <- c(
+  "Bangsan-Myeon", "Cheongnim-Dong", "Gwanak-Gu", "Hwasun-Eup", "Hyeoksin-Dong",
+  "Nowon-Gu", "Onui-Dong", "Pado-Ri", "Paju", "Saemangeum",
+  "Saesol-Dong", "Sangjusi", "Seolseong-Myeon", "Seonggeoeup", "Soi-Myeon",
+  "Taeha-Ri"
+)
+
+dados_filtrados <- dataset %>%
+  filter(Country %in% c(" South Korea"))
+
+dataset <- dados_filtrados
