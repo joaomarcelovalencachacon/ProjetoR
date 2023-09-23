5280fc24dcea8661cb68e0001c79fec8809d7952
Flávio Roberto <124847034+FlavioRobertoTavares@users.noreply.github.com>
2023-09-22 22:50:12 -0300

    Add nossa parte

--- ui.R
+++ ui.R
@@ -1,3 +1,4 @@
+
 library(shiny)
 library(shinydashboard)
 library(DT)
@@ -12,40 +13,78 @@ sidebar <- dashboardSidebar(
 )
 
 body <- dashboardBody(
-  tabItem(tabName = 'c',
-          fluidRow(
-            box(title = 'Selecione 2 classes',
-                width=12,
-                solidHeader = TRUE,
-                status = 'info',
-                selectInput('column_c', 'Dados', columns, multiple=TRUE),
-                selectInput('selected_city', 'Cidade', cities, multiple=FALSE),
-                uiOutput("timedate_c"),
-                actionButton('finish_c', 'Avançar')
+  tabItems(
+    tabItem(tabName = 'c',
+            fluidRow(
+              box(title = 'Selecione 2 classes',
+                  width=12,
+                  solidHeader = TRUE,
+                  status = 'info',
+                  selectInput('column_c', 'Dados', columns, multiple=TRUE),
+                  selectInput('selected_city', 'Cidade', cities, multiple=FALSE),
+                  uiOutput("timedate_c"),
+                  actionButton('finish_c', 'Avançar')
+              )
+            ),
+            fluidRow(
+              box(title = "Correlação entre as colunas", width = 12, solidHeader = TRUE,
+                  DTOutput('info_correlation')
+              )
+            ),
+            fluidRow(
+              box(title = "Gráfico de linha", width = 12, solidHeader = TRUE,
+                  plotOutput('linha')
+              )
+            ),
+            fluidRow(
+              box(title = "Grafico de barra média", width = 12, solidHeader = TRUE,
+                  plotOutput('barras_media')
+              )
+            ),
+            fluidRow(
+              box(title = "Grafico de dispersão", width = 12, solidHeader = TRUE,
+                  plotOutput('dispersao')
+              )
             )
-          ),
-          fluidRow(
-            box(title = "Correlação entre as colunas", width = 12, solidHeader = TRUE,
-                DTOutput('info_correlation')
+            
+    ),
+    tabItem(tabName = 'm',
+            fluidRow(
+              box(title = 'Selecione a classe a ser analisada',
+                  width=12,
+                  solidHeader = TRUE,
+                  status = 'info',
+                  selectInput('column_m', 'Dados', columns, multiple=FALSE),
+                  selectInput('selected_city_m', 'Cidade', cities, multiple=FALSE),
+                  uiOutput("timedate_m"),
+                  actionButton('finish_m', 'Avançar')
+              )
+            ),
+            fluidRow(
+              box(title = "Tabela", width = 12, solidHeader = TRUE,
+                  DTOutput('info_m')
+            ),
+            fluidRow(
+              box(title = "Gráfico em linha da série", width = 12, solidHeader = TRUE,
+                  plotOutput('linha_m')
+              )
+            ),
+            fluidRow(
+              box(title = "Histograma da série", width = 12, solidHeader = TRUE,
+                  plotOutput('hist_m')
+              )
+            ),
+            fluidRow(
+              box(title = "Boxplot da série.", width = 12, solidHeader = TRUE,
+                  plotOutput('boxplot_m')
+              )
             )
-          ),
-          fluidRow(
-            box(title = "Gráfico de linha", width = 12, solidHeader = TRUE,
-                plotOutput('linha')
-            )
-          ),
-          fluidRow(
-            box(title = "Grafico de barra média", width = 12, solidHeader = TRUE,
-                plotOutput('barras_media')
-            )
-          ),
-          fluidRow(
-            box(title = "Grafico de dispersão", width = 12, solidHeader = TRUE,
-                plotOutput('dispersao')
-            )
-          )
-          
+    )
+    
+    
   )
+    
+)
 )
 
 ui <- dashboardPage(
